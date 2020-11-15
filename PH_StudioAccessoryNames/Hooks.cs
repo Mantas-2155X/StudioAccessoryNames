471a9302693d091dc15264f5bcd018739239f2a3
Mantas-2155X <syenergyy@gmail.com>
2020-11-16 01:53:59 +0200

    * PH Support

---
+++ PH_StudioAccessoryNames/Hooks.cs
@@ -0,0 +1,21 @@
+using HarmonyLib;
+
+using Studio;
+
+namespace PH_StudioAccessoryNames
+{
+    public static class Hooks
+    {
+        [HarmonyPostfix, HarmonyPatch(typeof(MPCharCtrl.AccessoriesInfo), "UpdateInfo")]
+        private static void AccessoriesInfo_UpdateInfo_ChangeLabels(OCIChar _char)
+        {
+            PH_StudioAccessoryNames.instance.StartCoroutine(PH_StudioAccessoryNames.UpdateStudioLabelsDelayed(_char));
+        }
+        
+        [HarmonyPostfix, HarmonyPatch(typeof(MPCharCtrl), "UpdateInfo")]
+        private static void MPCharCtrl_UpdateInfo_ChangeLabels(OCIChar ___m_OCIChar)
+        {
+            PH_StudioAccessoryNames.instance.StartCoroutine(PH_StudioAccessoryNames.UpdateStudioLabelsDelayed(___m_OCIChar));
+        }
+    }
+}
---
+++ PH_StudioAccessoryNames/PH_StudioAccessoryNames.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+
+using BepInEx;
+using HarmonyLib;
+
+using Studio;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PH_StudioAccessoryNames
+{
+    [BepInPlugin(nameof(PH_StudioAccessoryNames), nameof(PH_StudioAccessoryNames), VERSION)]
+    [BepInProcess("PlayHomeStudio64bit")]
+    [BepInProcess("PlayHomeStudio32bit")]
+    public class PH_StudioAccessoryNames : BaseUnityPlugin
+    {
+        public const string VERSION = "1.0.0";
+        
+        public static PH_StudioAccessoryNames instance;
+
+        private void Awake()
+        {
+            instance = this;
+
+            var harmony = new Harmony("PH_StudioAccessoryNames");
+            harmony.PatchAll(typeof(Hooks));
+        }
+
+        public static IEnumerator UpdateStudioLabelsDelayed(OCIChar _char)
+        {
+            yield return null;
+
+            var slots = GameObject.Find("StudioScene/Canvas Main Menu/02_Manipulate/00_Chara/01_State/Viewport/Content/Slot");
+            var moreAccs = slots.transform.childCount > 10;
+            
+            for (var i = 0; i < slots.transform.childCount; i++)
+            {
+                var child = slots.transform.GetChild(i);
+
+                var buttons = child.GetComponentsInChildren<Button>();
+                foreach (var btn in buttons)
+                {
+                    var oldPos = btn.transform.localPosition;
+
+                    switch ((int)oldPos.x)
+                    {
+                        case 80:
+                            btn.transform.localPosition = new Vector3(104, oldPos.y, oldPos.z);
+                            break;
+                        case 104:
+                            btn.transform.localPosition = new Vector3(128, oldPos.y, oldPos.z);
+                            break;
+                    }
+                }
+                
+                var text = child.GetComponentInChildren<Text>();
+                if (text == null)
+                    continue;
+
+                text.resizeTextMinSize = 6;
+
+                var textRect = text.GetComponent<RectTransform>();
+                textRect.offsetMax = new Vector2(100, textRect.offsetMax.y);
+                
+                if (moreAccs && i == slots.transform.childCount - 1)
+                    continue;
+
+                if (!child.gameObject.activeSelf)
+                {
+                    text.text = $"スロット{i + 1:D2}";
+                }
+                else
+                {
+                    var acc = _char.charInfo.human.accessories.GetAccessoryData(_char.charInfo.human.customParam.acce, i);
+                    text.text = acc == null ? $"スロット{i+1:D2}" : $"{i+1:D2} {acc.name}";
+                }
+            }
+        }
+    }
+}
